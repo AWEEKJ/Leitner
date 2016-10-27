322260f99e99f7981a903395a463ee47eb9c8a8f
Hanju Jo <gkswn121@gmail.com>
2016-10-27 19:07:47 +0900

    Modify Build Settings and fix some errors
    1. compileSdkVersion을 25로 변경
    2. minSdkVersion을 22로 변경
    3. compile dependency 추가
    4. 앱 실행시 초기 진입 액티비티 변경

--- app/src/main/java/com/uos/leitner/MainActivity.java
+++ app/src/main/java/com/uos/leitner/MainActivity.java
@@ -5,24 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import static com.uos.leitner.R.layout.login;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_main);
 
-        //Intent intent = new Intent(this, login.class);
-        //startActivity(intent);
-        //setContentView(R.layout.activity_main);
     }
 
-    public void onClick(View view)
-    {
-        Intent intent = new Intent(this, LoginActivity.class);
-
-        startActivity(intent);
-    }
 }
