27a242bfac6e013cc647cfb0735910d456ca085b
Hanju Jo <gkswn121@gmail.com>
2016-11-04 10:14:09 +0900

    Fix bugs in LoginActivity

--- app/src/main/java/com/uos/leitner/LoginActivity.java
+++ app/src/main/java/com/uos/leitner/LoginActivity.java
@@ -3,17 +3,30 @@ package com.uos.leitner;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 /*
  * Created by JungJee on 2016. 10. 24..
  */
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginBtn = (Button) findViewById(R.id.loginButton);
+
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void onClick(View view) {
@@ -21,4 +34,5 @@ public class LoginActivity extends Activity {
 
         startActivity(intent);
     }
+
 }
